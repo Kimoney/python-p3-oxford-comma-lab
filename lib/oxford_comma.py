797ec49f54c7cf4002ce13de8685c1f89a8b79e4
John <johntaykimani@gmail.com>
2024-03-06 10:50:41 +0300

    Completed Assignment

--- lib/oxford_comma.py
+++ lib/oxford_comma.py
@@ -1,2 +1,14 @@
 def oxford_comma(items):
-    return None
+    if len(items) == 1:
+        return "".join(items)
+    elif len(items) == 2:
+        return " and ".join(items)
+    elif len(items) > 2:
+        string_with_commas = ", ".join(items[0:-1])
+        print(string_with_commas)
+        string_with_and = ", and "+items[-1]
+        print(string_with_commas + string_with_and)
+        return string_with_commas + string_with_and
+
+
+oxford_comma(["fiddleheads", "okra", "fiddleheads", "okra", "kohlrabi""kohlrabi"])
